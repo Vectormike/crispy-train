6b336e408cd006460a43a240def29f4c58aab0f7
Victor Jonah <victorjonah199@gmail.com>
2021-09-18 21:03:19 +0100

    chore: redis setup

--- src/config/redis.js
+++ src/config/redis.js
@@ -1,10 +1,48 @@
-const tokenTypes = {
-  ACCESS: 'access',
-  REFRESH: 'refresh',
-  RESET_PASSWORD: 'resetPassword',
-  VERIFY_EMAIL: 'verifyEmail',
+/* eslint-disable no-unused-expressions */
+const redis = require('redis');
+const { redisHost, redisPort } = require('./config');
+const logger = require('./logger');
+
+const client = redis.createClient();
+
+const set = (key, value) => {
+  client.setex(key, 200, JSON.stringify(value));
 };
 
-module.exports = {
-  tokenTypes,
+const get = (req, res, next) => {
+  const key = req.route.path;
+  client.get(key, (error, data) => {
+    if (error) res.status(400).send(err);
+    if (data !== null) res.status(200).send(JSON.parse(data));
+    else next();
+  });
 };
+
+const publish = (req, res, next) => {
+  const body = {
+    message: 'hello',
+  };
+
+  const data = JSON.stringify(req.body.message) || JSON.stringify(body);
+
+  const client = redis.createClient();
+
+  client.publish('uLesson-topic', data, (error, data) => {
+    console.log('Data:', data);
+    if (error) res.status(400).send(error);
+    if (data !== null) res.sendStatus(200).send(JSON.parse(data));
+    else next();
+  });
+};
+
+const subscribe = (req, res, next) => {
+  const client = redis.createClient();
+
+  client.on('message', (channel, message) => {
+    logger.info('Recieved data:' + message);
+  });
+
+  client.subscribe('uLesson-topic');
+};
+
+module.exports = { set, get, publish, subscribe };
