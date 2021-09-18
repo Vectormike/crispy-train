8c7b3f2c496ea16dd1a9e02785d5553c9719dc44
Victor Jonah <victorjonah199@gmail.com>
2021-09-18 21:02:50 +0100

    chore: server setup

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
-const mongoose = require('mongoose');
 const app = require('./app');
 const config = require('./config/config');
 const logger = require('./config/logger');
+const { subscribe } = require('./config/redis');
 
 let server;
-mongoose.connect(config.mongoose.url, config.mongoose.options).then(() => {
-  logger.info('Connected to MongoDB');
-  server = app.listen(config.port, () => {
-    logger.info(`Listening to port ${config.port}`);
-  });
+server = app.listen(config.port, () => {
+  logger.info(`Listening to port ${config.port}`);
 });
+
+subscribe('ulesson-topic');
 
 const exitHandler = () => {
   if (server) {
